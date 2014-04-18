eddba0188fe7f2fe1f701d16d15567a113c686d0
Matt Broadstone <mbroadstone@devonit.com>
2014-04-17 21:37:04 -0400

    move yelp code into choices router

--- server/routes/choices.js
+++ server/routes/choices.js
@@ -1,20 +1,22 @@
 var express = require('express'),
-		choices = express.Router(),
-		fakeData = require('./fake-data');
+    choices = express.Router(),
+    fakeData = require('./fake-data'),
+    config = require('../server-config');
 
+var yelp = require("yelp").createClient(config.yelpAuthInfo);
 choices.get('/', function(req, res) {
-	/*
-	yelp.search({term: "food", location: "19406"}, function(error, data) {
-	  if (!!error) {
-	  	res.json(error);
-	  	return;
-	  }
+  /*
+  yelp.search({term: "food", location: "19406"}, function(error, data) {
+    if (!!error) {
+      res.json(error);
+      return;
+    }
 
-	  res.json(data);
-	});
-	*/
+    res.json(data);
+  });
+  */
 
-	res.json(fakeData);
+  res.json(fakeData);
 });
 
 module.exports = choices;
